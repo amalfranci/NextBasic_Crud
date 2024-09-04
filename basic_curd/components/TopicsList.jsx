3550fa61562d5048d7085e6838f2321401749f9e
amal francis <amalfrancis744@gmail.com>
2024-09-04 12:28:43 +0530

    some style changed

--- basic_curd/components/TopicsList.jsx
+++ basic_curd/components/TopicsList.jsx
@@ -1,51 +1,45 @@
-import React from 'react'
-import Button from './Button'
-import Link from 'next/link'
-import {HiPencilAlt} from "react-icons/hi";
+import React from "react";
+import Button from "./Button";
+import Link from "next/link";
+import { HiPencilAlt } from "react-icons/hi";
 
-
-const getTopics= async()=>
-{
-  try{
-    const res = await fetch('http://localhost:3000/api/topics',{
-      cache:'no-store'
-    })
-    if(!res.ok){
-      throw new Error("Failed to fetch topics")
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
     }
-    return await res.json()
-  }catch(error)
-  {
-    console.log("error to load topics",error)
+    return await res.json();
+  } catch (error) {
+    console.log("error to load topics", error);
     return null; // Return null if there's an error
   }
-}
+};
 
 async function TopicsList() {
+  const data = await getTopics();
+  const { topics } = await data;
 
-
-  const data = await getTopics()
-const { topics} = await data
-    
   return (
     <>
-  {topics.map(t=>(
-    <div className='flex justify-between p-2 mt-3 border '>
-        <div>
-            <h2>{t.title}</h2>
+      {topics.map((t) => (
+        <div className="flex justify-between p-2 mt-3 border ">
+          <div>
+            <h2 className="font-bold">{t.title}</h2>
             <div>{t.description}</div>
+          </div>
+          <div className="flex gap-6 p-3">
+            <Button id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
-        <div className='flex gap-6 p-3'>
-        <Button id={t._id}/>
-        <Link href={`/editTopic/${t._id}`}>
-            <HiPencilAlt size={24}/>
-        </Link>
-          
-        </div>
-    </div>
-  ))}
+      ))}
     </>
-  )
+  );
 }
 
-export default TopicsList
+export default TopicsList;
